fddca4b7b1fcf90b05807783e70a7aa3fd83647a
tsmame767 <tsmane789@gmail.com>
2024-02-05 06:40:45 +0530

    commit1

---
+++ List/Classlist.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    public class Class1
+    {
+        public void List(List<int> list)                   //Adding the elements to list
+        {
+            int sum = 0;
+            foreach (var element in list)
+            {
+                sum += element;
+            }
+            Console.WriteLine("sum :"+sum);
+        }
+
+
+    }
+
+    public class Class2                                        //Remove Duplicates From a List
+    {
+        public void List(List<int> list)
+        {
+            List<int> uniqueList = new List<int>();
+            foreach (var element in list)
+            {
+                if (!uniqueList.Contains(element))
+                {
+                    uniqueList.Add(element);
+                }
+            }
+            foreach (var element in uniqueList)
+            {
+                Console.Write(element);
+            }
+            Console.WriteLine();
+
+        }
+    }
+
+    public class Class3                                           //Check if a list is sorted in ascending order
+    {
+        public void List(List<int> list)
+        {
+            bool isSorted = true;
+            for (int i = 0; i < (list.Count - 1); i++)
+            {
+                if (list[i] > list[i + 1])
+                {
+                    isSorted = false;
+                    break;
+                }
+            }
+            Console.Write("+ list is :"+isSorted+"\n");
+        }
+    }
+
+
+    public class Class4
+    {
+        public void List(List<int> list)                                //Reverse a given list:
+        {
+            for(int i=list.Count-1; i>=0; i--)
+            {
+                Console.Write(list[i] + " ");
+            }
+        }
+    }
+
+    public class Class5                                // Merge two lists into a single sorted list
+    {
+        public void List(List<int> list1, List<int> list2)
+        {
+            List<int> mergedList = new List<int>(list1);
+            foreach (var element in list2)
+            {
+                int index = 0;
+                while (index < mergedList.Count && element > mergedList[index])
+                {
+                    index++;
+                }
+                mergedList.Insert(index, element);
+            }
+
+            foreach (var element in mergedList)
+            {
+                Console.Write(element);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public class Class6
+    {
+        public void List(List<int> list)    //List Rotation
+        {
+            int positionsToRotate = 3;
+            for (int i = 0; i < positionsToRotate; i++)
+            {
+                int lastElement = list[list.Count - 1];
+                for (int j = list.Count - 1; j > 0; j--)
+                {
+                    list[j] = list[j - 1];
+                }
+                list[0] = lastElement;
+            }
+
+            foreach (var element in list)
+            {
+                Console.Write(element);
+            }
+            Console.WriteLine();
+
+        }
+    }
+
+    public class Class7                 //count occurenece
+    {
+        public void List(List<int> list)
+        {
+            Dictionary<int, int> occurrences = new Dictionary<int, int>();
+            foreach (var element in list)
+            {
+                if (occurrences.ContainsKey(element))
+                {
+                    occurrences[element]++;
+                }
+                else
+                {
+                    occurrences[element] = 1;
+                }
+            }
+
+        }
+
+    }
+}
---
+++ List/main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List
+{
+    internal class main
+    {
+        static void Main()
+        {
+            List<int> list1 = [4, 3, 6, 8, 1, 2, 9];
+            List<int> list2 = [30, 20, 40, 80, 60, 30];
+
+            Class1 user1 = new Class1();
+            user1.List(list1);
+
+            Class2 user2 = new Class2();
+            user2.List(list1);
+
+            Class3 user3 = new Class3();
+            user3.List(list1);
+
+            Class4 user4 = new Class4();
+            user4.List(list1);
+
+            Class5 user5 = new Class5();
+            user5.List(list1, list2);
+
+            Class6 user6 = new Class6();
+            user6.List(list1);
+
+            Class7 user7 = new Class7();
+            user7.List(list1);
+
+
+
+        }
+    }
+}
