fb1e26b9819b13a3cf5695c564b1ba9be76b3800
tsmame767 <tsmane789@gmail.com>
2024-02-05 08:03:07 +0530

    commit3

---
+++ Dict/ConsoleApp1/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Dict
+    {
+        Dictionary<string,int> occ = new Dictionary<string,int>();
+        string[] strings;
+
+         
+
+        public void wordcount(string[] name )
+        {
+            
+            foreach(string i in name)
+            {
+                if (occ.ContainsKey(i))
+                {
+                    occ[i]++;
+                }
+                else
+                {
+                    occ[i] = 1;
+                }
+            }
+            foreach(var c in occ)
+            {
+                string key = c.Key;
+                int val = c.Value;
+                Console.WriteLine($"{key} : { val}");
+            }
+        }
+
+        public void charcount(string[] chars)
+        {
+            foreach(string c in chars)
+            {
+                if (occ.ContainsKey(c))
+                {
+                    occ[c]++;
+                }
+                else
+                {
+                    occ[c] = 1;
+                }
+            }
+
+            foreach (var c in occ)
+            {
+                string key = c.Key;
+                int val = c.Value;
+                Console.WriteLine($"{key} : {val}");
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ Dict/ConsoleApp1/main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class main
+    {
+        static void Main(){
+        string[] name1 = { "tushar", "hemant", "vikas", "tushar" };
+        string[] name2 = { "a","c","a","e","i","a"};
+        Dict obj = new Dict();
+
+        //obj.wordcount(name);
+        obj.charcount(name2);
+         }
+
+    }
+}
